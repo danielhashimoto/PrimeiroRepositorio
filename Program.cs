ecf0dac69e713c53dbd6be27df2c7d82f4084897
DANIEL MERLIM HASHIMOTO <daniel.mhashimoto@senacsp.edu.br>
2018-04-11 08:53:57 -0300

    adição de variáveis e captura de dados

--- Program.cs
+++ Program.cs
@@ -6,11 +6,28 @@ namespace NewGit
     {
         static void Main(string[] args)
         {
-            
-            
+        
+        int dia;
+        int mes;
+        int ano;
+
+        Console.WriteLine("Insira o dia utilizando 2 dígitos");
+        dia = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o mês utilizando 2 dígitos");
+        mes = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o ano utilizando 2 dígitos");
+        ano = int.Parse(Console.ReadLine());
 
 
-            
+
+
+
+
+
+
+
         }
     }
 }
